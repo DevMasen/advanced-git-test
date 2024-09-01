b990066a701f4515c010c978fe44ce2c09b8b8b4
DevMasen <mohammadwh400@gmail.com>
2024-09-01 23:32:10 +0330

    stash conflict resolved

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const useVar = function () {
 	console.log(first + second + third);
 };
 
-const fromMainBranch = function(){
-    return false;
-}
+const fromMainBranch = function () {
+	return false;
+};
 // Some code for future use
-const future = function(){
-    return 'Fucked up ';
-}
+const future = function () {
+	return "Fucked up ";
+};
+
+const stashVar1 = 11;
+const stashVar2 = 12;
+const stashVar3 = 13;
 
 const stashVar4 = 14;
 const stashVar5 = 15;
